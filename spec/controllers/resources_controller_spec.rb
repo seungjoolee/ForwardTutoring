c86552384d6df05a895eb3ccca7536a960d2aae3
Kent Nguyen <kentingnguyen@yahoo.com>
2013-11-04 16:47:02 -0800

    added spec tests

--- spec/controllers/resources_controller_spec.rb
+++ spec/controllers/resources_controller_spec.rb
@@ -1,11 +1,21 @@
 require 'spec_helper'
 describe ResourcesController do
-  describe "Adding a resource" do
-    it "should error with a blank title"
+  describe "Create a resource" do
+    it "should return an error with a blank title" do
+     # Resource.stub(:create).and_return(nil)
+     # post :new, {:resource_author => "Kent"}
+      end
     it "should pick only 1 of the link or the file"
 
-    context "posting a link" do
-    it "should error with a blank link"
+    context "happy path posting a link" do
+      it "should make a new resource in the database" do
+        mock_resource = mock('resource', :id => 5, :title => "Algebra", :link => "algebra.com")
+        Resource.should_receive(:create!).and_return(mock_resource)
+        post :create #, {:title => mock_resource.title}
+      end
+
+       context "error with a blank link" do
+        end
     end
     context "happy path upload a file" do
 
--- spec/models/resource_spec.rb
+++ spec/models/resource_spec.rb
@@ -1,5 +1,17 @@
 require 'spec_helper'
 describe Resource do
-  it "should error with a blank title"
+
+  it "should error with a nil title", pending: true do
+    resource_hash = {title: "", author: "Kent", link: "google.com"}
+    lambda {Resource.create!(resource_hash)}.should raise_error(ValidationError)
+  end
+
+  it "should error with a blank title", pending: true do
+    resource_hash = {title: "", author: "Kent", link: "google.com"}
+    lambda {Resource.create!(resource_hash)}.should raise_error(ValidationError)
+    end
+
   it "should error with a blank link or file"
+   it "should pick only 1 of the link or the file"
+
 end
