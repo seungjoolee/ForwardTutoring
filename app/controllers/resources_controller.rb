dbbfef55e9a638a7054e9ee8141a803a64635830
Kent Nguyen <kentingnguyen@yahoo.com>
2013-11-08 01:46:58 -0800

    unable to do the error checking in resources_controller, sad path in cucumber, help

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -5,20 +5,29 @@ class ResourcesController < ApplicationController
 
   def new
     #render "new" template
+    if params[:resource].nil?
+      @resource_hash = Hash.new
+    else
+      @resource_hash[:title] = params[:resource_title]
+      @resource_hash[:description] = params[:resource_description]
+      @resource_hash[:posted_by] = params[:resource_posted_by]
+    end
   end
 
   def create
-    if params[:resource_title].nil?
-      flash[:notice] = "There is no title for this resource yet."
-      redirect_to new_resource_path and return
-    elsif params[:resource_link].nil?
-      flash[:notice] = "There is no link for this resource yet."
-      redirect_to new_resource_path and return
-    else
+    #if params[:resource_title].nil? or params[:resource_title].empty?
+    #  flash[:notice] = "There is no title for this resource yet."
+      # @resource_hash = {title: params[:resource_title], :posted_by => params[:resource_posted_by], description: params[:resource_description]}
+     # redirect_to new_resource_path(params[:resource]) and return
+    #elsif params[:resource_link].nil?
+    #  flash[:notice] = "There is no link for this resource yet."
+       # @resource_hash = {title: params[:resource_title], posted_by: params[:resource_posted_by], description: params[:resource_description]}
+    #  redirect_to new_resource_path(params[:resource]) and return
+   # else
     @resource = Resource.create!(params[:resource])
     #redirect_to resource_path(@resource.id)
     redirect_to resources_path
-    end
+   # end
   end
 
   def show
