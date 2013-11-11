4d5700c80092a4dd096e6f3ab267350b95e6d30d
Calvin Woo <calvin.woo@citrix.com>
2013-11-10 19:57:29 -0800

    Seed update

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,14 +7,28 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 math  = Subject.create(:title => "Math")
-geometry = math.fields.create(:title => "Geometry")
-triangles = geometry.topics.create(:title => "Triangles")
-squares = geometry.topics.create(:title => "Circumference")
-algebra = math.fields.create(:title => "Algebra")
+  geometry = math.fields.create(:title => "Geometry")
+    proofs = geometry.topics.create(:title => "Proofs")
+    lines = geometry.topics.create(:title => "Parallel and Perpendicular Lines")
+    triangles = geometry.topics.create(:title => "Triangles")
+    quadrilaterals = geometry.topics.create(:title => "Quadrilaterals")
+    proportions_and_similarity = geometry.topics.create(:title => "Proportions and Similarity")
+    right_triangles_and_trigonometry = geometry.topics.create(:title => "Right Triangles and Trigonometry")
+    transformations = geometry.topics.create(:title => "Transformations")
+    circles = geometry.topics.create(:title => "Circles")
+    areas_of_polygons_and_circles = geometry.topics.create(:title => "Areas of Polygons and Circles")
+    surface_area_and_volume = geometry.topics.create(:title => "Surface Area and Volume")
+  algebra_i = math.fields.create(:title => "Algebra I")
+  algebra_ii = math.fields.create(:title => "Algebra II")
+  calculus_i = math.fields.create(:title => "Calculus I")
+  calculus_ii = math.fields.create(:title => "Calculus II")
 
-
-
-#subjects = Subject.create([ {title: "Math"}, {title: "Science"}, {title: "History"} ])
-#math_fields = Field.create([ {:title => "Calculus"}, {:title => "Algebra"}, {:title => "Geometry"} ])
-#math.fields = math_fields
-#subjects.first.fields = math_fields
+science = Subject.create(:title => "Science")
+  biology = science.fields.create(:title => "Biology")
+  chemistry = science.fields.create(:title => "Chemistry")
+  physics = science.fields.create(:title => "Physics")
+  
+#history = Subject.create(:title => "History")
+#english = Subject.create(:title => "English")
+#foreign_languages = Subject.create(:title => "Foreign Languages")
+#art_and_music = Subject.create(:title => "Art and Music")
