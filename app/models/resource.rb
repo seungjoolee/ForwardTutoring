52d7ca176f7fc4350f41b59b4e001ed6ac15d7a4
miumiulin <0926588303@yahoo.com.tw>
2013-11-10 16:54:59 -0800

    merge

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,6 +1,5 @@
 class Resource < ActiveRecord::Base
-	has_many :reviews
-	
+  has_many :reviews
   attr_accessible :title, :posted_by, :description, :link, :category
   belongs_to :topic
   validates_presence_of :title, :link, :category, {:message => "should not be blank"}
