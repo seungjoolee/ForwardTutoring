d1a5d5a5a547627f8329dcf92488c7b5e04dbb59
Calvin Woo <calvin.woo@citrix.com>
2013-11-10 20:00:25 -0800

    Migration AddDescriptionToTopics

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131111030532) do
+ActiveRecord::Schema.define(:version => 20131111035855) do
 
   create_table "fields", :force => true do |t|
     t.string   "title"
@@ -38,9 +38,10 @@ ActiveRecord::Schema.define(:version => 20131111030532) do
 
   create_table "topics", :force => true do |t|
     t.string   "title"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
     t.integer  "field_id"
+    t.text     "description"
   end
 
 end
