3c30e0027985006c189310913f93994872735c17
gothvz <goth.ting@hotmail.com>
2013-11-10 16:48:10 -0800

    create and show comments

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,7 +2,7 @@ class ReviewsController < ApplicationController
 	def create
 		@resource = Resource.find(params[:resource_id])
 		@review = @resource.reviews.create(params[:review])
-		redirect_to resources_path(@resource)
+		redirect_to resource_path(@resource)
 	end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,19 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-
-ActiveRecord::Schema.define(:version => 20131110005829) do
-
-  create_table "fields", :force => true do |t|
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
+ActiveRecord::Schema.define(:version => 20131109193122) do
 
   create_table "resources", :force => true do |t|
     t.string   "title"
-    t.string   "author"
-    t.string   "summary"
+    t.string   "posted_by"
+    t.string   "category"
     t.text     "description"
+    t.string   "link"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
   end
@@ -39,15 +34,5 @@ ActiveRecord::Schema.define(:version => 20131110005829) do
   end
 
   add_index "reviews", ["resource_id"], :name => "index_reviews_on_resource_id"
-
-  create_table "subjects", :force => true do |t|
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
-  create_table "topics", :force => true do |t|
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
 
 end
