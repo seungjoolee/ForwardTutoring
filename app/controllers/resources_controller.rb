37aae9bfc154b391fd08fcb9bd7a1abb3f962621
saasbook <saasbook@saasbook.(none)>
2013-11-24 20:53:02 -0800

    Gemfile.lock

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -1,12 +1,13 @@
 class ResourcesController < ApplicationController
-  def index
-    if params[:topic].nil?
-      @resources = Resource.all
-    else
-      topic = params[:topic]
-      @resources = Resource.where("category == ?", topic)
-    end
-  end
+  ##
+  #def index
+  #  if params[:topic].nil?
+  #    @resources = Resource.all
+  #  else
+  #    topic = params[:topic]
+  #    @resources = Resource.where("category == ?", topic)
+  #  end
+  #end
 
   def new
     #render "new" template
