286a218896e747d72d649890aa0009a67398bced
Kent Nguyen <kentingnguyen@yahoo.com>
2013-12-07 18:18:06 -0800

    commit

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -4,6 +4,13 @@ class Resource < ActiveRecord::Base
   belongs_to :topic
 
   has_attached_file :pdf , url: "/pdfs/:id", default_url: ""
+  validates_attachment_size :pdf, :less_than => 10.megabytes
+  validates_attachment_content_type :pdf, :content_type => ["text/plain",
+                                                            "application/vnd.ms-excel",
+                                                            "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+                                                            "application/msword",
+                                                            "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+                                                            "application/pdf"], :message => 'Only PDF, WORD, or TEXT files are allowed. '
 
   validates_presence_of :title, :topic_id, {:message => "should not be blank"}
 #  validates_presence_of :link, {:message => "should not be blank"}
