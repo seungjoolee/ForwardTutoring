58799134ced9cd51c157f7aa1a2d08b2ec3e669d
miumiulin <0926588303@yahoo.com.tw>
2013-12-07 21:50:17 -0800

    added content length checking

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,5 @@ class Review < ActiveRecord::Base
   belongs_to :resource
   attr_accessible :anonymous, :content, :rating, :user, :resource_id
   validates_presence_of :user, :content,  {:message => "should not be blank"}
+  validates :content, length: { maximum: 150, too_long: "%{count} chars is the maximum allowed"}
 end
