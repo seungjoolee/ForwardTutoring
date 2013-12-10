fa150c6c0820a59f293a85f68af044ba064845c1
exiao <exiao@berkeley.edu>
2013-12-10 01:03:47 -0800

    add uid to dr_user, change find_by_name authenticate

--- app/models/dr_user.rb
+++ app/models/dr_user.rb
@@ -1,12 +1,12 @@
 class DrUser < ActiveRecord::Base
   # attr_accessible :title, :body
-  attr_accessible :mail, :pass
+  attr_accessible :mail, :pass, :uid
 
   has_many :resources
   has_many :reviews
 
-  def self.authenticate(email, password)
-    user = DrUser.find_by_name(email)
+  def self.authenticate(name, password)
+    user = DrUser.find_by_name(name)
     if user && user.pass == Digest::MD5.hexdigest(password)
       user
     else
