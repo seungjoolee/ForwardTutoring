3ee6d5ea7e7b6a9a25c6779bfe4d06dfee1be066
Calvin Woo <calvin.woo@citrix.com>
2013-12-09 21:39:59 -0800

    User associations to review and resource.

--- app/models/dr_user.rb
+++ app/models/dr_user.rb
@@ -1,6 +1,10 @@
 class DrUser < ActiveRecord::Base
   # attr_accessible :title, :body
   attr_accessible :mail, :pass
+
+  has_many :resources
+  has_many :reviews
+
   def self.authenticate(email, password)
     user = DrUser.find_by_name(email)
     if user && user.pass == Digest::MD5.hexdigest(password)
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,9 @@
 class Review < ActiveRecord::Base
-  belongs_to :resource
   attr_accessible :anonymous, :content, :rating, :user, :resource_id
   validates_presence_of :user, :content,  {:message => "should not be blank"}
   validates :content, length: { maximum: 150, too_long: "%{count} chars is the maximum allowed"}
+
+  belongs_to :resource
+  belongs_to :dr_user
+
 end
