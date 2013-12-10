e689be0d690ecc83114f3f3826ee77a898501ddf
exiao <exiao@berkeley.edu>
2013-12-09 21:02:23 -0800

    user authentication

--- app/models/dr_user.rb
+++ app/models/dr_user.rb
@@ -1,3 +1,12 @@
 class DrUser < ActiveRecord::Base
   # attr_accessible :title, :body
+  attr_accessible :mail, :pass
+  def self.authenticate(email, password)
+    user = DrUser.find_by_name(email)
+    if user && user.pass == Digest::MD5.hexdigest(password)
+      user
+    else
+      nil
+    end
+  end
 end
