1ce2ff0124cc5ab0896c7f25c14a94371fa19e2d
exiao <exiao@berkeley.edu>
2014-02-25 21:47:06 -0800

    final commit to fix chosen

--- app/assets/javascripts/new.js
+++ app/assets/javascripts/new.js
@@ -1,19 +1,19 @@
-$(document).ready(function(){ 
-	var SelectFile = function() {
-		$("#link-group").hide();
-		$("#select-link").removeClass("active");
-		$("#file-group").show();
-		$("#select-file").addClass("active");
-		$(".submit-buttons").show();
-	}
 
-	var SelectLink = function() {
-		$("#file-group").hide();
-		$("#select-file").removeClass("active");
-		$("#link-group").show();
-		$("#select-link").addClass("active");
-		$(".submit-buttons").show();
-	}
-	console.log("ADW");
+var SelectFile = function() {
+	$("#link-group").hide();
+	$("#select-link").removeClass("active");
+	$("#file-group").show();
+	$("#select-file").addClass("active");
+	$(".submit-buttons").show();
+}
+
+var SelectLink = function() {
+	$("#file-group").hide();
+	$("#select-file").removeClass("active");
+	$("#link-group").show();
+	$("#select-link").addClass("active");
+	$(".submit-buttons").show();
+}
+$(document).ready(function(){ 
 	$("#resource_topic_id").chosen();
 });
